eb6062289981e2578f4a0dc110dfbb09ccb81b6f
Saqibahmed13 <saqibcode@gmail.com>
2024-12-27 18:21:30 +0530

    added one question

--- TallySolutionProject/index.js
+++ TallySolutionProject/index.js
@@ -1,3 +1,18 @@
-buttonClick = () => {
-    alert("you have clicked me!!!")
-}
+document.getElementById('clickMeBtn').addEventListener('click', ()=> {
+    // Hide the "Click Me" button
+    document.getElementById('clickMeBtn').style.display = 'none';
+    
+    // Show the confirmation card
+    document.getElementById('confirmationCard').style.display = 'block';
+});
+
+document.getElementById('confirmYes').addEventListener('click', function() {
+    alert('You clicked Yes!');
+    // Hide the confirmation card
+    document.getElementById('confirmationCard').style.display = 'none';
+});
+
+document.getElementById('confirmNo').addEventListener('click', function() {
+    // Hide the confirmation card
+    document.getElementById('confirmationCard').style.display = 'none';
+});
